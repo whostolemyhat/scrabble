eb11ee34a284e5fe433072d1ee6127e8d8d14f3d
James Baum <james@jamesbaum.co.uk>
2019-11-28 15:18:11 +0000

    Add cli arg handling

---
+++ scrabble/src/generate.rs
@@ -0,0 +1,51 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::collections::HashMap;
+use std::error::Error;
+
+pub type Dictionary = HashMap<String, Vec<String>>;
+
+#[derive(Debug)]
+pub enum Wordlist {
+  Sowpods,
+  Twl
+}
+
+pub fn sort_letters(word: &str) -> String {
+  let mut chars: Vec<char> = word.chars().collect();
+  chars.sort();
+  let ordered = chars.into_iter().collect::<String>();
+  ordered
+}
+
+pub fn generate_dict(list: &Wordlist) -> Result<(), Box<dyn Error>> {
+  let mut map: Dictionary = HashMap::new();
+
+  let source = match list {
+    Wordlist::Sowpods => "sowpods.txt",
+    Wordlist::Twl => "twl06.txt"
+  };
+
+  let file = File::open(source)?;
+  let reader = BufReader::new(file);
+
+  for line in reader.lines() {
+    let line = line?;
+    let ordered = sort_letters(line.as_str());
+
+    map.entry(ordered).and_modify(|m| m.push(line.clone())).or_insert(vec![line.clone()]);
+  }
+
+  let out_path = format!("{:?}-dict.json", list);
+  let mut output = File::create(out_path)?;
+  let json = serde_json::to_string(&map)?;
+  output.write_all(json.as_bytes())?;
+
+  Ok(())
+}
+
+pub fn load_dict(list: &Wordlist) -> Result<Dictionary, Box<dyn Error>> {
+  let dict_file = File::open(format!("{:?}-dict.json", list))?;
+  let dict = serde_json::from_reader(dict_file)?;
+  Ok(dict)
+}
---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use std::io::{stdin};
+use std::error::Error;
+use clap::{App, Arg};
+// use std::path::Path;
+use log::{info};
+
+use scrabble::{find_all};
+use scrabble::generate::{Wordlist, generate_dict, load_dict};
+
+// anagrammer
+// 1words
+// TODO switch json/cbor
+// TODO arg to generate
+// TODO est score
+// TODO gen module
+// Todo length > 1
+// TODO length < 10?
+// TODO sort by length
+fn main() -> Result<(), Box<dyn Error>> {
+  env_logger::init();
+
+  let matches = App::new("Scrabble")
+    .version("1.0")
+    .author("James Baum <@whostolemyhat>")
+    .about("Finds allowed words for scrabble-type games")
+    .arg(Arg::with_name("wordlist")
+      .short("w")
+      .long("wordlist")
+      .takes_value(true)
+      .possible_values(&["sowpods", "twl"])
+      .default_value("sowpods")
+      .help("Wordlist to use. TWL for US/Thailand, SOWPODS for rest of the world (English-speaking)"))
+    .subcommand(
+      App::new("generate")
+        .about("Parses word list and creates map of anagrams")
+        .arg(Arg::with_name("wordlist")
+          .short("w")
+          .long("wordlist")
+          .takes_value(true)
+          .possible_values(&["sowpods", "twl"])
+          .default_value("sowpods")
+          .help("Wordlist to use. TWL for US/Thailand, SOWPODS for rest of the world (English-speaking)"))
+    )
+    .get_matches();
+
+  if let Some(ref matches) = matches.subcommand_matches("generate") {
+    // different list in generate
+    let list = match matches.value_of("wordlist").expect("Must provide a word list") {
+      "sowpods" => Wordlist::Sowpods,
+      "twl" => Wordlist::Twl,
+      _ => unreachable![]
+    };
+    generate_dict(&list)?;
+    return Ok(());
+  };
+
+  let list = match matches.value_of("wordlist").expect("Must provide a word list") {
+    "sowpods" => Wordlist::Sowpods,
+    "twl" => Wordlist::Twl,
+    _ => unreachable![]
+  };
+
+  let dict = load_dict(&list)?;
+
+  // replace with info!
+  info!("Loaded dict");
+  let mut found: Vec<String>;
+
+  loop {
+    let mut input_text = String::new();
+    stdin()
+      .read_line(&mut input_text)
+      .expect("Failed to read input");
+
+    if input_text.trim() == "q" {
+      info!("Exiting");
+      break;
+    }
+
+    found = find_all(&dict, &input_text.trim());
+
+    println!("{:?} {:?}", found, found.len());
+  }
+
+  Ok(())
+}
